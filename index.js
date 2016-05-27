e287b60da9a947deb9061adbda444176d56bed7f
Nate Benz <nb@chrios.net>
2016-05-26 20:59:01 -0400

    Added base website files

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile('public/index.html');
+});
+
+var HOST = process.env.HOST || '127.0.0.1';
+var PORT = process.env.PORT || '4000';
+app.listen(PORT, HOST, function() {
+  console.log('Serving website on ' + HOST + ':' + PORT);
+});
